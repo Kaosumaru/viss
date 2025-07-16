4e1c9f359b5b906b924406b189bb5f449cf6b3ad
Mateusz Borycki <mateuszborycki@gmail.com>
2025-07-16 10:35:11 +0200

    Update

--- src/compiler/compiler/nodes/basic/literal.ts
+++ src/compiler/compiler/nodes/basic/literal.ts
@@ -7,9 +7,6 @@ export class LiteralNode<T extends ScalarType> extends CompilerNode {
     super();
     this.type = type;
 
-    // Add input for value (so it can be connected)
-    this.inputs.value = type;
-
     // Add parameter for value (so it can be manually entered)
     this.parameters.value = {
       type: "number",
@@ -19,20 +16,12 @@ export class LiteralNode<T extends ScalarType> extends CompilerNode {
     this.outputs.out = type;
   }
   override compile(node: NodeContext): Context {
-    // Try to get input first, fall back to parameter
-    const inputContext = node.tryGetInput("value");
-    if (inputContext) {
-      // If there's a connection, use the input
-      return inputContext;
-    } else {
-      // If no connection, use the parameter value
-      const value = node.getParamValue("value", "number");
-      const v = value.toLocaleString("en-GB", { minimumFractionDigits: 1 });
-      return {
-        type: this.type,
-        mainOutput: v,
-      };
-    }
+    const value = node.getParamValue("value", "number");
+    const v = value.toLocaleString("en-GB", { minimumFractionDigits: 1 });
+    return {
+      type: this.type,
+      mainOutput: v,
+    };
   }
 
   override getLabel(): string {
--- src/compiler/compiler/nodes/vector/composeVector4.ts
+++ src/compiler/compiler/nodes/vector/composeVector4.ts
@@ -1,5 +1,9 @@
 import { scalar, vector } from "@glsl/types";
-import { CompilerNode, type NodeContext } from "../compilerNode";
+import {
+  CompilerNode,
+  type NodeContext,
+  type ParameterInfo,
+} from "../compilerNode";
 import type { Context } from "@compiler/context";
 
 class ComposeVector4 extends CompilerNode {
@@ -9,23 +13,41 @@ class ComposeVector4 extends CompilerNode {
     this.inputs.y = scalar("float");
     this.inputs.z = scalar("float");
     this.inputs.w = scalar("float");
-    this.parameters.x = {
+
+    const param: ParameterInfo = {
       type: "number",
       defaultValue: { type: "number", value: 0 },
     };
+    this.parameters.x = param;
+    this.parameters.y = param;
+    this.parameters.z = param;
+    this.parameters.w = param;
     this.outputs.out = vector("float", 4);
   }
 
   override compile(node: NodeContext): Context {
-    const ctxX = node.getInput("x");
-    const ctxY = node.getInput("y");
-    const ctxZ = node.getInput("z");
-    const ctxW = node.getInput("w");
+    const x = this.getInputOrParamValue(node, "x");
+    const y = this.getInputOrParamValue(node, "y");
+    const z = this.getInputOrParamValue(node, "z");
+    const w = this.getInputOrParamValue(node, "w");
 
     return {
       type: this.outputs.out,
-      mainOutput: `vec4(${ctxX.mainOutput}, ${ctxY.mainOutput}, ${ctxZ.mainOutput}, ${ctxW.mainOutput})`,
+      mainOutput: `vec4(${x}, ${y}, ${z}, ${w})`,
     };
+  }
+
+  getInputOrParamValue(node: NodeContext, name: string): string {
+    if (node.hasInput(name)) {
+      const input = node.getInput(name);
+      if (input.type.id === "scalar" && input.type.type === "float") {
+        return input.mainOutput;
+      }
+      throw new Error(`Input ${name} is not of type number`);
+    } else {
+      const value = node.getParamValue(name, "number");
+      return value.toLocaleString("en-GB", { minimumFractionDigits: 1 });
+    }
   }
 
   override getLabel(): string {
